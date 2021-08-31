5806e44ea46514a885dc9633ca2f646ed3dc5ff1
Caroline A. McKeon <cannemckeon@gmail.com>
2021-08-31 11:41:31 -0700

    Update m_hamiltonian_twobodies.f90

--- src/m_hamiltonian_twobodies.f90
+++ src/m_hamiltonian_twobodies.f90
@@ -974,7 +974,7 @@ end subroutine setup_exchange_ri_cmplx
 ! Calculate the exchange-correlation potential and energy
 ! * subroutine works for both real and complex wavefunctions c_matrix
 !   using "class" syntax of Fortran2003
-subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,exc_ao)
+subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc)
  implicit none
 
  integer,intent(in)             :: batch_size
@@ -984,7 +984,7 @@ subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,
  real(dp),intent(out)           :: vxc_ao(:,:,:)
  real(dp),intent(out)           :: exc_xc
  !begin CMK
- real(dp),intent(out),optional  :: exc_ao(:,:,:)
+ !real(dp),intent(out),optional  :: exc_ao(:,:,:)
  !end CMK
 !=====
  real(dp),parameter   :: TOL_RHO=1.0e-9_dp
@@ -997,8 +997,8 @@ subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,
  real(dp),allocatable :: weight_batch(:)
  real(dp),allocatable :: tmp_batch(:,:)
  !Begin CMK
- real(dp),allocatable :: exc_ao_batch(:)
- real(dp),allocatable :: tmp_exc_batch(:,:)
+ !real(dp),allocatable :: exc_ao_batch(:)
+ !real(dp),allocatable :: tmp_exc_batch(:,:)
  !End CMK
  real(dp),allocatable :: basis_function_r_batch(:,:)
  real(dp),allocatable :: bf_gradx_batch(:,:)
@@ -1019,7 +1019,7 @@ subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,
  exc_xc = 0.0_dp
  vxc_ao(:,:,:) = 0.0_dp
  !Begin CMK
- exc_ao(:,:,:) = 0.0_dp
+ !exc_ao(:,:,:) = 0.0_dp
  !End CMK
 
  write(stdout,*) 'CMK dft routine 2'
@@ -1239,8 +1239,8 @@ subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,
      !Create e_xc matrix in ao basis
      
      !initialize matrices
-     exc_ao_batch(:) = 0.0_dp
-     tmp_exc_batch(:,:) = 0.0_dp
+     !exc_ao_batch(:) = 0.0_dp
+     !tmp_exc_batch(:,:) = 0.0_dp
 
       ! Create temporary exc matrix that updates with batch
      !exc_ao_batch(:) = exc_ao_batch(:) + weight_batch(:) * exc_batch(:) * SUM(rhor_batch(:,:),DIM=1) * dft_xc(ixc)%coeff
