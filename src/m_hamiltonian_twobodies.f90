cba0702c7b99a54af14e4414d25ea4569e08e918
Caroline A. McKeon <cannemckeon@gmail.com>
2021-08-31 16:22:18 -0700

    Debug

--- src/m_hamiltonian_twobodies.f90
+++ src/m_hamiltonian_twobodies.f90
@@ -1246,15 +1246,15 @@ subroutine dft_exc_vxc_batch(batch_size,basis,occupation,c_matrix,vxc_ao,exc_xc,
      exc_ao_batch(:) = exc_ao_batch(:) + weight_batch(:) * exc_batch(:) * SUM(rhor_batch(:,:),DIM=1) * dft_xc(ixc)%coeff
      
       ! Send exc matrix to ao basis representation
-     !do ir=1,nr
-      !  tmp_exc_batch(:,ir) = SQRT( MAX(exc_ao_batch(:),1.1e-15_dp) ) * basis_function_r_batch(:,ir)
-     !enddo
+     do ir=1,nr
+        tmp_exc_batch(:,ir) = SQRT( MAX(exc_ao_batch(:),1.1e-15_dp) ) * basis_function_r_batch(:,ir)
+     enddo
 
      !Get an nbf x nbf matrix
      !call DSYRK('L','N',basis%nbf,nr,-1.0d0,tmp_exc_batch,basis%nbf,1.0d0,exc_ao(:,:,ispin),basis%nbf)
 
-     !deallocate(tmp_exc_batch)
-     !deallocate(exc_ao_batch)
+     deallocate(tmp_exc_batch)
+     deallocate(exc_ao_batch)
 
      !End CMK
 
